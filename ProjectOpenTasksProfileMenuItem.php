a4a1bba0dae1ece6a3517cd1631c52a562ed0f34
MacFan4000 <paulfkeffer@gmail.com>
2017-12-16 11:14:29 -0500

    Add Open tasks item for project menus (#2)
    * Add Open tasks item for project menus
    
    This would be helpful for projects that have the workboard disabled.
    
    * Update ProjectOpenTasksProfileMenuItem.php

---
+++ ProjectOpenTasksProfileMenuItem.php
@@ -0,0 +1,43 @@
+<?php
+final class ProjectOpenTasksProfileMenuItem
+  extends PhabricatorProfileMenuItem {
+  const MENUITEMKEY = 'custom.open-tasks';
+  public function getMenuItemTypeIcon() {
+    return 'fa-anchor';
+  }
+  public function getMenuItemTypeName() {
+    return pht('Link to Open Tasks');
+  }
+  public function canAddToObject($object) {
+    return ($object instanceof PhabricatorProject);
+  }
+  public function getDisplayName(
+    PhabricatorProfileMenuItemConfiguration $config) {
+    return pht('Open Tasks');
+  }
+  protected function newNavigationMenuItems(
+    PhabricatorProfileMenuItemConfiguration $config) {
+    $object = $config->getProfileObject();
+    $href = '/maniphest/?project='.$object->getPHID().'&statuses=open()&group=none&order=newest#R';
+    $item = $this->newItem()
+      ->setHref($href)
+      ->setName($this->getDisplayName($config))
+      ->setIcon('fa-anchor');
+    return array(
+      $item,
+    );
+  }
+  public function buildEditEngineFields(
+    PhabricatorProfileMenuItemConfiguration $config) {
+    return array(
+      id(new PhabricatorInstructionsEditField())
+        ->setValue(
+          pht(
+            'This adds a link to search maniphest for open tasks which are '.
+            "tagged with this project.\n\n".
+            "NOTE: This feature is provided by a Miraheze-maintained ".
+            'extension, ProjectOpenTasksProfileMenuItemextension. See '.
+            '{rPHEX} for the source.')),
+    );
+  }
+}
