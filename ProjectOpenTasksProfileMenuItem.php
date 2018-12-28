c56285adb62e888c4c371ca2a4956d7be85ebbc1
paladox <paladox@users.noreply.github.com>
2018-12-28 01:44:40 +0000

    Update ProjectOpenTasksProfileMenuItem.php

--- ProjectOpenTasksProfileMenuItem.php
+++ ProjectOpenTasksProfileMenuItem.php
@@ -1,5 +1,7 @@
 <?php
 
+// Source: https://raw.githubusercontent.com/wikimedia/phabricator-extensions/wmf/stable/src/panel/ProjectOpenTasksProfileMenuItem.php
+
 final class ProjectOpenTasksProfileMenuItem
   extends PhabricatorProfileMenuItem {
 
