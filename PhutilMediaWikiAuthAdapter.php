cba5f1408488ae1f13287b129301f5f30c1e2257
paladox <paladox@users.noreply.github.com>
2018-12-28 01:44:09 +0000

    Update PhutilMediaWikiAuthAdapter.php

--- PhutilMediaWikiAuthAdapter.php
+++ PhutilMediaWikiAuthAdapter.php
@@ -1,5 +1,7 @@
 <?php
 
+// Source: https://raw.githubusercontent.com/wikimedia/phabricator-extensions/wmf/stable/src/oauth/PhutilMediaWikiAuthAdapter.php
+
 /**
  * Authentication adapter for MediaWiki OAuth1.
  */
