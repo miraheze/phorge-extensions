0e4473f1e45b4628715cb64bcdb258bb75906f5a
paladox <paladox@users.noreply.github.com>
2018-12-28 01:40:55 +0000

    Update ProjectOpenTasksProfileMenuItem.php

--- ProjectOpenTasksProfileMenuItem.php
+++ ProjectOpenTasksProfileMenuItem.php
@@ -1,32 +1,54 @@
 <?php
+
 final class ProjectOpenTasksProfileMenuItem
   extends PhabricatorProfileMenuItem {
+
   const MENUITEMKEY = 'custom.open-tasks';
+
   public function getMenuItemTypeIcon() {
     return 'fa-anchor';
   }
+
+  public function getDefaultName() {
+    return pht('Open Tasks');
+  }
+
   public function getMenuItemTypeName() {
     return pht('Link to Open Tasks');
   }
-  public function canAddToObject($object) {
-    return ($object instanceof PhabricatorProject);
+
+  public function canHideMenuItem(
+    PhabricatorProfileMenuItemConfiguration $config) {
+    return true;
   }
+
+  public function canMakeDefault(
+    PhabricatorProfileMenuItemConfiguration $config) {
+    return false;
+  }
+
   public function getDisplayName(
     PhabricatorProfileMenuItemConfiguration $config) {
-    return pht('Open Tasks');
+    return $this->getDefaultName();
   }
+
   protected function newNavigationMenuItems(
     PhabricatorProfileMenuItemConfiguration $config) {
+
     $object = $config->getProfileObject();
+
     $href = '/maniphest/?project='.$object->getPHID().'&statuses=open()&group=none&order=newest#R';
+
     $item = $this->newItem()
       ->setHref($href)
       ->setName($this->getDisplayName($config))
       ->setIcon('fa-anchor');
+
     return array(
       $item,
     );
   }
+
   public function buildEditEngineFields(
     PhabricatorProfileMenuItemConfiguration $config) {
     return array(
@@ -34,10 +56,8 @@ final class ProjectOpenTasksProfileMenuItem
         ->setValue(
           pht(
             'This adds a link to search maniphest for open tasks which are '.
-            "tagged with this project.\n\n".
-            "NOTE: This feature is provided by a Miraheze-maintained ".
-            'extension, ProjectOpenTasksProfileMenuItemextension. See '.
-            '{rPHEX} for the source.')),
+            "tagged with this project.\n\n")),
     );
   }
+
 }
