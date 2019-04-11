8f79666a11b4ff0ad4bcf2f7e63ec5e8374fe444
paladox <paladox@users.noreply.github.com>
2019-04-11 01:36:31 +0100

    Fix support for latest phabricator update

--- ProjectOpenTasksProfileMenuItem.php
+++ ProjectOpenTasksProfileMenuItem.php
@@ -34,15 +34,15 @@ final class ProjectOpenTasksProfileMenuItem
     return $this->getDefaultName();
   }
 
-  protected function newNavigationMenuItems(
+  protected function newMenuItemViewList(
     PhabricatorProfileMenuItemConfiguration $config) {
 
     $object = $config->getProfileObject();
 
-    $href = '/maniphest/?project='.$object->getPHID().'&statuses=open()&group=none&order=newest#R';
+    $uri = '/maniphest/?project='.$object->getPHID().'&statuses=open()&group=none&order=newest#R';
 
-    $item = $this->newItem()
-      ->setHref($href)
+    $item = $this->newItemView()
+      ->setURI($uri)
       ->setName($this->getDisplayName($config))
       ->setIcon('fa-anchor');
 
