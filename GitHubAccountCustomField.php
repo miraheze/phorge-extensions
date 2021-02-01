be1392c5a84845461f23b864eca502b21736d1ed
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-02-01 10:27:45 -0700

    Fix tabbing

--- GitHubAccountCustomField.php
+++ GitHubAccountCustomField.php
@@ -1,144 +1,144 @@
 <?php
 
 final class GitHubAccountCustomField extends PhabricatorUserCustomField {
-  protected $externalAccount;
+	protected $externalAccount;
 
-  public function shouldUseStorage() {
-    return false;
-  }
+	public function shouldUseStorage() {
+		return false;
+	}
 
-  public function getFieldKey() {
-    return 'github:externalaccount';
-  }
+	public function getFieldKey() {
+		return 'github:externalaccount';
+	}
 
-  public function getFieldName() {
-    return pht( "GitHub User" );
-  }
+	public function getFieldName() {
+		return pht( "GitHub User" );
+	}
 
-  public function getFieldValue() {
-    $account = $this->getExternalAccount();
+	public function getFieldValue() {
+		$account = $this->getExternalAccount();
 
-    if ( !$account || !strlen( $account->getAccountURI() ) ) {
-      return null;
-    }
+		if ( !$account || !strlen( $account->getAccountURI() ) ) {
+			return null;
+		}
 
-    $uri = urldecode( $account->getAccountURI() );
+		$uri = urldecode( $account->getAccountURI() );
 
-    // Split on the User: part of the userpage uri
-    $name = explode( "github.com/", $uri );
-    // grab the part after User:
-    $name = array_pop( $name );
-    // decode for display:
-    $name = urldecode( rawurldecode( $name ) );
+		// Split on the User: part of the userpage uri
+		$name = explode( "github.com/", $uri );
+		// grab the part after User:
+		$name = array_pop( $name );
+		// decode for display:
+		$name = urldecode( rawurldecode( $name ) );
 
-    return $name;
-  }
+		return $name;
+	}
 
-  protected function getExternalAccount() {
-    if ( !$this->externalAccount ) {
-      $user = $this->getObject();
-      $this->externalAccount = id( new PhabricatorExternalAccount() )->loadOneWhere(
-        'userPHID = %s AND accountType = %s',
-        $user->getPHID(),
-        'github' );
-    }
-    return $this->externalAccount;
-  }
+	protected function getExternalAccount() {
+		if ( !$this->externalAccount ) {
+			$user = $this->getObject();
+			$this->externalAccount = id( new PhabricatorExternalAccount() )->loadOneWhere(
+				'userPHID = %s AND accountType = %s',
+				$user->getPHID(),
+				'github' );
+		}
+		return $this->externalAccount;
+	}
 
-  public function shouldAppearInPropertyView() {
-    return true;
-  }
+	public function shouldAppearInPropertyView() {
+		return true;
+	}
 
-  public function renderPropertyViewLabel() {
-    return $this->getFieldName();
-  }
+	public function renderPropertyViewLabel() {
+		return $this->getFieldName();
+	}
 
-  public function renderPropertyViewValue( array $handles ) {
+	public function renderPropertyViewValue( array $handles ) {
 
-    $account = $this->getExternalAccount();
+		$account = $this->getExternalAccount();
 
-    if ( !$account || !strlen( $account->getAccountURI() ) ) {
-      return pht( 'Unknown' );
-    }
+		if ( !$account || !strlen( $account->getAccountURI() ) ) {
+			return pht( 'Unknown' );
+		}
 
-    $uri = urldecode( $account->getAccountURI() );
+		$uri = urldecode( $account->getAccountURI() );
 
-    // Split on the User: part of the userpage uri
-    $name = explode( "github.com/", $uri );
-    // grab the part after User:
-    $name = array_pop( $name );
-    // decode for display:
-    $name = urldecode( rawurldecode( $name ) );
+		// Split on the User: part of the userpage uri
+		$name = explode( "github.com/", $uri );
+		// grab the part after User:
+		$name = array_pop( $name );
+		// decode for display:
+		$name = urldecode( rawurldecode( $name ) );
 
-    return phutil_tag(
-      'a',
-      [
-        'href' => $uri
-      ],
-      $name );
-  }
-
-
-  public function shouldAppearInApplicationSearch() {
-    return true;
-  }
+		return phutil_tag(
+			'a',
+			[
+				'href' => $uri
+			],
+			$name );
+	}
 
 
-  public function getFieldType() {
-    return 'text';
-  }
+	public function shouldAppearInApplicationSearch() {
+		return true;
+	}
 
-  public function buildFieldIndexes() {
-    $indexes = [];
 
-    $value = $this->getFieldValue();
-    if ( strlen( $value ) ) {
-      $indexes[] = $this->newStringIndex( $value );
-      $indexes[] = $this->newStringIndex( urldecode( $this->getExternalAccount()->getAccountURI() ) );
-      $parts = explode( ' ', $value );
-      if ( count( $parts ) > 1 ) {
-        foreach( $parts as $part ) {
-          $indexes[] = $this->newStringIndex( $part );
-        }
-      }
-    }
+	public function getFieldType() {
+		return 'text';
+	}
 
-    return $indexes;
-  }
+	public function buildFieldIndexes() {
+		$indexes = [];
 
-  public function readApplicationSearchValueFromRequest(
-    PhabricatorApplicationSearchEngine $engine,
-    AphrontRequest $request ) {
+		$value = $this->getFieldValue();
+		if ( strlen( $value ) ) {
+			$indexes[] = $this->newStringIndex( $value );
+			$indexes[] = $this->newStringIndex( urldecode( $this->getExternalAccount()->getAccountURI() ) );
+			$parts = explode( ' ', $value );
+			if ( count( $parts ) > 1 ) {
+				foreach( $parts as $part ) {
+					$indexes[] = $this->newStringIndex( $part );
+				}
+			}
+		}
 
-    return $request->getStr( $this->getFieldKey() );
-  }
+		return $indexes;
+	}
 
-  public function applyApplicationSearchConstraintToQuery(
-    PhabricatorApplicationSearchEngine $engine,
-    PhabricatorCursorPagedPolicyAwareQuery $query,
-    $value ) {
+	public function readApplicationSearchValueFromRequest(
+		PhabricatorApplicationSearchEngine $engine,
+		AphrontRequest $request ) {
 
-    if ( strlen( $value ) ) {
-      $query->withApplicationSearchContainsConstraint(
-        $this->newStringIndex( null ),
-        $value );
-    }
-  }
+		return $request->getStr( $this->getFieldKey() );
+	}
 
-  public function appendToApplicationSearchForm(
-    PhabricatorApplicationSearchEngine $engine,
-    AphrontFormView $form,
-    $value ) {
+	public function applyApplicationSearchConstraintToQuery(
+		PhabricatorApplicationSearchEngine $engine,
+		PhabricatorCursorPagedPolicyAwareQuery $query,
+		$value ) {
 
-    $form->appendChild(
-      id( new AphrontFormTextControl() )
-        ->setLabel( $this->getFieldName() )
-        ->setName( $this->getFieldKey() )
-        ->setValue( $value ) );
-  }
+		if ( strlen( $value ) ) {
+			$query->withApplicationSearchContainsConstraint(
+				$this->newStringIndex( null ),
+				$value );
+		}
+	}
 
-  protected function newStringIndexStorage() {
-    return new PhabricatorUserCustomFieldStringIndex();
-  }
+	public function appendToApplicationSearchForm(
+		PhabricatorApplicationSearchEngine $engine,
+		AphrontFormView $form,
+		$value ) {
+
+		$form->appendChild(
+			id( new AphrontFormTextControl() )
+				->setLabel( $this->getFieldName() )
+				->setName( $this->getFieldKey() )
+				->setValue( $value ) );
+	}
+
+	protected function newStringIndexStorage() {
+		return new PhabricatorUserCustomFieldStringIndex();
+	}
 
 }
