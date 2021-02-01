bda69829ef443aed99444361e8f0a8ac3675e5bf
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-02-01 11:02:26 -0700

    Fix tabbing

--- ProjectOpenTasksProfileMenuItem.php
+++ ProjectOpenTasksProfileMenuItem.php
@@ -2,64 +2,66 @@
 
 // Source: https://raw.githubusercontent.com/wikimedia/phabricator-extensions/wmf/stable/src/panel/ProjectOpenTasksProfileMenuItem.php
 
-final class ProjectOpenTasksProfileMenuItem
-  extends PhabricatorProfileMenuItem {
+final class ProjectOpenTasksProfileMenuItem extends PhabricatorProfileMenuItem {
+	const MENUITEMKEY = 'custom.open-tasks';
 
-  const MENUITEMKEY = 'custom.open-tasks';
+	public function getMenuItemTypeIcon() {
+		return 'fa-anchor';
+	}
 
-  public function getMenuItemTypeIcon() {
-    return 'fa-anchor';
-  }
+	public function getDefaultName() {
+		return pht( 'Open Tasks' );
+	}
 
-  public function getDefaultName() {
-    return pht( 'Open Tasks' );
-  }
+	public function getMenuItemTypeName() {
+		return pht( 'Link to Open Tasks' );
+	}
 
-  public function getMenuItemTypeName() {
-    return pht( 'Link to Open Tasks' );
-  }
+	public function canHideMenuItem(
+		PhabricatorProfileMenuItemConfiguration $config
+	) {
+		return true;
+	}
 
-  public function canHideMenuItem(
-    PhabricatorProfileMenuItemConfiguration $config ) {
-    return true;
-  }
+	public function canMakeDefault(
+		PhabricatorProfileMenuItemConfiguration $config
+	) {
+		return false;
+	}
 
-  public function canMakeDefault(
-    PhabricatorProfileMenuItemConfiguration $config ) {
-    return false;
-  }
+	public function getDisplayName(
+		PhabricatorProfileMenuItemConfiguration $config
+	) {
+		return $this->getDefaultName();
+	}
 
-  public function getDisplayName(
-    PhabricatorProfileMenuItemConfiguration $config ) {
-    return $this->getDefaultName();
-  }
+	protected function newMenuItemViewList(
+		PhabricatorProfileMenuItemConfiguration $config
+	) {
+		$object = $config->getProfileObject();
 
-  protected function newMenuItemViewList(
-    PhabricatorProfileMenuItemConfiguration $config ) {
+		$uri = '/maniphest/?project=' . $object->getPHID() . '&statuses=open()&group=none&order=newest#R';
 
-    $object = $config->getProfileObject();
+		$item = $this->newItemView()
+			->setURI( $uri )
+			->setName( $this->getDisplayName( $config ) )
+			->setIcon( 'fa-anchor' );
 
-    $uri = '/maniphest/?project=' . $object->getPHID() . '&statuses=open()&group=none&order=newest#R';
+		return [
+			$item,
+		];
+	}
 
-    $item = $this->newItemView()
-      ->setURI( $uri )
-      ->setName( $this->getDisplayName( $config ) )
-      ->setIcon( 'fa-anchor' );
-
-    return [
-      $item,
-    ];
-  }
-
-  public function buildEditEngineFields(
-    PhabricatorProfileMenuItemConfiguration $config ) {
-    return [
-      id( new PhabricatorInstructionsEditField() )
-        ->setValue(
-          pht(
-            'This adds a link to search maniphest for open tasks which are '.
-            "tagged with this project.\n\n" ) ),
-    ];
-  }
-
+	public function buildEditEngineFields(
+		PhabricatorProfileMenuItemConfiguration $config
+	) {
+		return [
+			id( new PhabricatorInstructionsEditField() )->setValue(
+				pht(
+					'This adds a link to search maniphest for open tasks which are '.
+					"tagged with this project.\n\n"
+				)
+			),
+		];
+	}
 }
