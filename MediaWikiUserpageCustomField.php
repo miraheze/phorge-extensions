8dad4fb35b16deef2ecffd87617cd655988d25af
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-02-02 01:22:03 -0700

    Use tabs not spaces (#9)
    * Fix tabbing
    
    * Fix tabbing
    
    * fix formatting
    
    * Fix tabbing
    
    * Fix tabbing
    
    * Fix tabbing
    
    * Fix spacing
    
    * Fix formatting
    
    * fix spacing
    
    * fix spacing

--- MediaWikiUserpageCustomField.php
+++ MediaWikiUserpageCustomField.php
@@ -3,149 +3,149 @@
 // Source: https://raw.githubusercontent.com/wikimedia/phabricator-extensions/wmf/stable/src/customfields/MediaWikiUserpageCustomField.php
 
 final class MediaWikiUserpageCustomField extends PhabricatorUserCustomField {
-  protected $externalAccount;
+	protected $externalAccount;
 
-  public function shouldUseStorage() {
-    return false;
-  }
+	public function shouldUseStorage() {
+		return false;
+	}
 
-  public function getFieldKey() {
-    return 'mediawiki:externalaccount';
-  }
+	public function getFieldKey() {
+		return 'mediawiki:externalaccount';
+	}
 
-  public function getFieldName() {
-    return pht( "Miraheze User" );
-  }
+	public function getFieldName() {
+		return pht( "Miraheze User" );
+	}
 
-  public function getFieldValue() {
-    $account = $this->getExternalAccount();
+	public function getFieldValue() {
+		$account = $this->getExternalAccount();
 
-    if ( !$account || !strlen( $account->getAccountURI() ) ) {
-      return null;
-    }
+		if ( !$account || !strlen( $account->getAccountURI() ) ) {
+			return null;
+		}
 
-    $uri = urldecode( $account->getAccountURI() );
+		$uri = urldecode( $account->getAccountURI() );
 
-    // Split on the User: part of the userpage uri
-    $name = explode( 'User:', $uri );
-    // grab the part after User:
-    $name = array_pop( $name );
-    // decode for display:
-    $name = urldecode( rawurldecode( $name ) );
+		// Split on the User: part of the userpage uri
+		$name = explode( 'User:', $uri );
+		// grab the part after User:
+		$name = array_pop( $name );
+		// decode for display:
+		$name = urldecode( rawurldecode( $name ) );
 
-    return $name;
-  }
+		return $name;
+	}
 
-  protected function getExternalAccount() {
-    if ( !$this->externalAccount ) {
-      $user = $this->getObject();
-      $this->externalAccount = id( new PhabricatorExternalAccount() )->loadOneWhere(
-        'userPHID = %s AND accountType = %s',
-        $user->getPHID(),
-        'mediawiki' );
-    }
-    return $this->externalAccount;
-  }
+	protected function getExternalAccount() {
+		if ( !$this->externalAccount ) {
+			$user = $this->getObject();
+			$this->externalAccount = id( new PhabricatorExternalAccount() )->loadOneWhere(
+				'userPHID = %s AND accountType = %s',
+				$user->getPHID(),
+				'mediawiki'
+			);
+		}
 
-  public function shouldAppearInPropertyView() {
-    return true;
-  }
+		return $this->externalAccount;
+	}
 
-  public function renderPropertyViewLabel() {
-    return $this->getFieldName();
-  }
+	public function shouldAppearInPropertyView() {
+		return true;
+	}
 
-  public function renderPropertyViewValue( array $handles ) {
+	public function renderPropertyViewLabel() {
+		return $this->getFieldName();
+	}
 
-    $account = $this->getExternalAccount();
+	public function renderPropertyViewValue( array $handles ) {
+		$account = $this->getExternalAccount();
 
-    if ( !$account || !strlen( $account->getAccountURI() ) ) {
-      return pht( 'Unknown' );
-    } else {
-      $userpage_uri = urldecode( $account->getAccountURI() );
-    }
+		if ( !$account || !strlen( $account->getAccountURI() ) ) {
+			return pht( 'Unknown' );
+		} else {
+			$userpage_uri = urldecode( $account->getAccountURI() );
+		}
 
-    // Split on the User: part of the userpage uri
-    $name = explode( 'User:', $userpage_uri );
-    // grab the part after User:
-    $rawname = array_pop( $name );
-    // decode for display:
-    $name = urldecode( rawurldecode( $rawname ) );
-    $accounts_uri = [ 'href' =>
-                      "https://meta.miraheze.org/wiki/Special:CentralAuth?target=" .
-                      $rawname ];
-    $accounts_text = pht( 'Global Accounts' );
-    $userpage_uri = [ 'href' => $userpage_uri ];
+		// Split on the User: part of the userpage uri
+		$name = explode( 'User:', $userpage_uri );
+		// grab the part after User:
+		$rawname = array_pop( $name );
+		// decode for display:
+		$name = urldecode( rawurldecode( $rawname ) );
+		$accounts_uri = [ 'href' =>
+				"https://meta.miraheze.org/wiki/Special:CentralAuth?target=" .
+				$rawname ];
+		$accounts_text = pht( 'Global Accounts' );
+		$userpage_uri = [ 'href' => $userpage_uri ];
 
-    return phutil_tag( 'span', [], [
-      phutil_tag( 'a', $userpage_uri, $name ),
-      ' [ ',
-      phutil_tag( 'a', $accounts_uri, $accounts_text ),
-      ' ]'
-    ] );
-  }
+		return phutil_tag( 'span', [], [
+			phutil_tag( 'a', $userpage_uri, $name ),
+			' [ ',
+			phutil_tag( 'a', $accounts_uri, $accounts_text ),
+			' ]'
+		] );
+	}
 
+	public function shouldAppearInApplicationSearch() {
+		return true;
+	}
 
-  public function shouldAppearInApplicationSearch() {
-    return true;
-  }
+	public function getFieldType() {
+		return 'text';
+	}
 
+	public function buildFieldIndexes() {
+		$indexes = [];
 
-  public function getFieldType() {
-    return 'text';
-  }
+		$value = $this->getFieldValue();
+		if ( strlen( $value ) ) {
+			$indexes[] = $this->newStringIndex( $value );
+			$indexes[] = $this->newStringIndex( urldecode( $this->getExternalAccount()->getAccountURI() ) );
+			$parts = explode( ' ', $value );
+			if ( count( $parts ) > 1 ) {
+				foreach( $parts as $part ) {
+					$indexes[] = $this->newStringIndex( $part );
+				}
+			}
+		}
 
-  public function buildFieldIndexes() {
-    $indexes = [];
+		return $indexes;
+	}
 
-    $value = $this->getFieldValue();
-    if ( strlen( $value ) ) {
-      $indexes[] = $this->newStringIndex( $value );
-      $indexes[] = $this->newStringIndex( urldecode( $this->getExternalAccount()->getAccountURI() ) );
-      $parts = explode( ' ', $value );
-      if ( count( $parts ) > 1 ) {
-        foreach( $parts as $part ) {
-          $indexes[] = $this->newStringIndex( $part );
-        }
-      }
-    }
+	public function readApplicationSearchValueFromRequest(
+		PhabricatorApplicationSearchEngine $engine,
+		AphrontRequest $request
+	) {
+		return $request->getStr( $this->getFieldKey() );
+	}
 
-    return $indexes;
-  }
+	public function applyApplicationSearchConstraintToQuery(
+		PhabricatorApplicationSearchEngine $engine,
+		PhabricatorCursorPagedPolicyAwareQuery $query,
+		$value
+	) {
+		if ( strlen( $value ) ) {
+			$query->withApplicationSearchContainsConstraint(
+				$this->newStringIndex( null ),
+				$value
+			);
+		}
+	}
 
-  public function readApplicationSearchValueFromRequest(
-    PhabricatorApplicationSearchEngine $engine,
-    AphrontRequest $request) {
+	public function appendToApplicationSearchForm(
+		PhabricatorApplicationSearchEngine $engine,
+		AphrontFormView $form,
+		$value
+	) {
+		$form->appendChild(
+			id( new AphrontFormTextControl() )
+				->setLabel( $this->getFieldName() )
+				->setName( $this->getFieldKey() )
+				->setValue( $value )
+		);
+	}
 
-    return $request->getStr( $this->getFieldKey() );
-  }
-
-  public function applyApplicationSearchConstraintToQuery(
-    PhabricatorApplicationSearchEngine $engine,
-    PhabricatorCursorPagedPolicyAwareQuery $query,
-    $value ) {
-
-    if ( strlen( $value ) ) {
-      $query->withApplicationSearchContainsConstraint(
-        $this->newStringIndex( null ),
-        $value );
-    }
-  }
-
-  public function appendToApplicationSearchForm(
-    PhabricatorApplicationSearchEngine $engine,
-    AphrontFormView $form,
-    $value ) {
-
-    $form->appendChild(
-      id( new AphrontFormTextControl() )
-        ->setLabel( $this->getFieldName() )
-        ->setName( $this->getFieldKey() )
-        ->setValue( $value ) );
-  }
-
-  protected function newStringIndexStorage() {
-    return new PhabricatorUserCustomFieldStringIndex();
-  }
-
+	protected function newStringIndexStorage() {
+		return new PhabricatorUserCustomFieldStringIndex();
+	}
 }
--- PhutilMediaWikiAuthAdapter.php
+++ PhutilMediaWikiAuthAdapter.php
@@ -5,215 +5,251 @@
 /**
  * Authentication adapter for MediaWiki OAuth1.
  */
-final class PhutilMediaWikiAuthAdapter
-    extends PhutilOAuth1AuthAdapter {
+final class PhutilMediaWikiAuthAdapter extends PhutilOAuth1AuthAdapter {
+	private $userinfo;
+	private $domain = '';
+	private $mediaWikiBaseURI = '';
+	private $callback_uri = '';
 
-  private $userinfo;
-  private $domain = '';
-  private $mediaWikiBaseURI = '';
-  private $callback_uri = '';
+	public function getWikiPageURI( $title, $query_params = null ) {
+		$uri = $this->mediaWikiBaseURI;
 
-  public function getWikiPageURI( $title, $query_params = null ) {
-    $uri = $this->mediaWikiBaseURI;
-    if ( substr( $uri, -1 ) != '/' ) {
-      $uri .= '/';
-    }
-    if ( !is_array( $query_params ) ) {
-      $query_params = [];
-    }
-    $query_params['title'] = $title;
-    return rawurldecode( $uri.'index.php?'.
-        http_build_query(
-          $query_params,
-          '',
-          '&' ) );
-  }
+		if ( substr( $uri, -1 ) != '/' ) {
+			$uri .= '/';
+		}
 
-  public function getAccountID() {
-    $this->getHandshakeData();
-    return idx( $this->getUserInfo(), 'userid' );
-  }
+		if ( !is_array( $query_params ) ) {
+			$query_params = [];
+		}
 
-  public function getAccountName() {
-    return idx( $this->getUserInfo(), 'username' );
-  }
+		$query_params['title'] = $title;
 
-  public function getAccountURI() {
-    $name = $this->getAccountName();
-    if ( strlen( $name ) ) {
-      return $this->getWikiPageURI( 'User:' . $name );
-    }
-    return null;
-  }
+		return rawurldecode( $uri . 'index.php?' .
+			http_build_query(
+				$query_params,
+				'',
+				'&'
+			)
+		);
+	}
 
-  public function getAccountImageURI() {
-    $info = $this->getUserInfo();
-    return idx( $info, 'profile_image_url' );
-  }
+	public function getAccountID() {
+		$this->getHandshakeData();
 
-  public function getAccountRealName() {
-    $info = $this->getUserInfo();
-    return idx( $info, 'name' );
-  }
+		return idx( $this->getUserInfo(), 'userid' );
+	}
 
-  public function getAdapterType() {
-    return 'mediawiki';
-  }
+	public function getAccountName() {
+		return idx( $this->getUserInfo(), 'username' );
+	}
 
-  public function getAdapterDomain() {
-    return $this->domain;
-  }
+	public function getAccountURI() {
+		$name = $this->getAccountName();
 
-  /* mediawiki oauth needs the callback uri to be "oob"
-   (out of band callback) */
-  public function getCallbackURI() {
-    return $this->callback_uri;
-  }
+		if ( strlen( $name ) ) {
+			return $this->getWikiPageURI( 'User:' . $name );
+		}
 
-  public function setCallbackURI( $uri ) {
-    $this->callback_uri = $uri;
-  }
+		return null;
+	}
 
-  public function shouldAddCSRFTokenToCallbackURI() {
-    return false;
-  }
+	public function getAccountImageURI() {
+		$info = $this->getUserInfo();
 
-  protected function getRequestTokenURI() {
-    $callback = $this->getCallbackURI();
-    return $this->getWikiPageURI( 'Special:OAuth/initiate',
-                  [ 'oauth_callback' => $callback ] );
-  }
+		return idx( $info, 'profile_image_url' );
+	}
 
-  protected function getAuthorizeTokenURI() {
-    return $this->getWikiPageURI( 'Special:OAuth/authorize' );
-  }
+	public function getAccountRealName() {
+		$info = $this->getUserInfo();
 
-  public function setAdapterDomain( $domain ) {
-    $this->domain = $domain;
-    return $this;
-  }
+		return idx( $info, 'name' );
+	}
 
-  public function setMediaWikiBaseURI( $uri ) {
-    $this->mediaWikiBaseURI = $uri;
-    return $this;
-  }
+	public function getAdapterType() {
+		return 'mediawiki';
+	}
 
-  public function getClientRedirectURI() {
-    $p = parent::getClientRedirectURI();
-    return $p. "&oauth_consumer_key={$this->getConsumerKey()}";
-  }
+	public function getAdapterDomain() {
+		return $this->domain;
+	}
 
-  protected function getValidateTokenURI() {
-    return $this->getWikiPageURI( 'Special:OAuth/token' );
-  }
+	/* mediawiki oauth needs the callback uri to be "oob"
+	 (out of band callback) */
+	public function getCallbackURI() {
+		return $this->callback_uri;
+	}
 
-  private function getUserInfo() {
-    if ( $this->userinfo === null ) {
-      $uri = new PhutilURI(
-        $this->getWikiPageURI( 'Special:OAuth/identify',
-                        [ 'format' => 'json' ] ) );
+	public function setCallbackURI( $uri ) {
+		$this->callback_uri = $uri;
+	}
 
-      // We gen this so we can check for replay below:
-      $nonce = Filesystem::readRandomCharacters( 32 );
-      list( $body ) = $this->newOAuth1Future( $uri )
-        ->setMethod( 'GET' )
-        ->setNonce( $nonce )
-        ->addHeader( 'User-Agent', __CLASS__ )
-        ->resolvex();
-      $this->userinfo = $this->decodeAndVerifyJWT( $body, $nonce );
-    }
-    return $this->userinfo;
-  }
+	public function shouldAddCSRFTokenToCallbackURI() {
+		return false;
+	}
 
-  protected function willProcessTokenRequestResponse( $body ) {
-    if ( substr_count( $body, 'Error:' ) > 0 ) {
-      throw new Exception(
-        pht( 'OAuth provider returned an error response.' ) );
-    }
-  }
+	protected function getRequestTokenURI() {
+		$callback = $this->getCallbackURI();
 
-  /**
-   * MediaWiki uses a signed JWT to assert the user's identity
-   * here we verify the identity, not just the jwt signature.
-   */
-  private function decodeAndVerifyJWT( $jwt, $nonce ) {
-    $userinfo = [];
-    $identity = $this->decodeJWT( $jwt );
-    $iss_uri = new PhutilURI( $identity->iss );
-    $expected_uri = new PhutilURI( $this->mediaWikiBaseURI );
+		return $this->getWikiPageURI( 'Special:OAuth/initiate',
+				[ 'oauth_callback' => $callback ]
+			);
+	}
 
-    $now = time();
+	protected function getAuthorizeTokenURI() {
+		return $this->getWikiPageURI( 'Special:OAuth/authorize' );
+	}
 
-    if ( $iss_uri->getDomain() !== $expected_uri->getDomain() ) {
-      throw new Exception(
-        pht( 'OAuth JWT iss didn\'t match expected server name' ) );
-    }
-    if ( $identity->aud !== $this->getConsumerKey() ) {
-      throw new Exception(
-        pht( 'OAuth JWT aud didn\'t match expected consumer key' ) );
-    }
-    if ( $identity->iat > $now || $identity->exp < $now ) {
-      throw new Exception(
-        pht( 'OAuth JWT wasn\'t valid at this time' ) );
-    }
-    if ( $identity->nonce !== $nonce ) {
-      throw new Exception(
-        pht( 'OAuth JWT nonce didn\'t match what we sent.' ) );
-    }
-    if ( $identity->blocked ) {
-      throw new Exception(
-        pht( 'OAuth error: this account has been blocked in MediaWiki.' ) );
-    }
+	public function setAdapterDomain( $domain ) {
+		$this->domain = $domain;
 
-    $userinfo['userid'] = $identity->sub;
-    $userinfo['username'] = $identity->username;
-    $userinfo['groups'] = $identity->groups;
-    $userinfo['blocked'] = $identity->blocked;
-    $userinfo['editcount'] = $identity->editcount;
-    return $userinfo;
-  }
+		return $this;
+	}
 
-  /** decode a JWT and verify the signature is valid */
-  private function decodeJWT( $jwt ) {
-    list( $headb64, $bodyb64, $sigb64 ) = explode( '.', $jwt );
+	public function setMediaWikiBaseURI( $uri ) {
+		$this->mediaWikiBaseURI = $uri;
 
-    $header = json_decode( $this->urlsafeB64Decode( $headb64 ) );
-    $body = json_decode( $this->urlsafeB64Decode( $bodyb64 ) );
-    $sig = $this->urlsafeB64Decode( $sigb64 );
+		return $this;
+	}
 
-    $expect_sig = hash_hmac(
-        'sha256',
-        "$headb64.$bodyb64",
-        $this->getConsumerSecret()->openEnvelope(),
-        true );
+	public function getClientRedirectURI() {
+		$p = parent::getClientRedirectURI();
 
-    // MediaWiki will only use sha256 hmac (HS256) for now.
-    // This checks that an attacker doesn't return invalid JWT signature type.
-    if ($header->alg !== 'HS256' ||
-        !$this->compareHash( $sig, $expect_sig ) ) {
-      throw new Exception( 'Invalid JWT signature from /identify.' );
-    }
+		return $p. "&oauth_consumer_key={$this->getConsumerKey()}";
+	}
 
-    return $body;
-  }
+	protected function getValidateTokenURI() {
+		return $this->getWikiPageURI( 'Special:OAuth/token' );
+	}
 
-  private function urlsafeB64Decode( $input ) {
-    $remainder = strlen( $input ) % 4;
-    if ( $remainder ) {
-      $padlen = 4 - $remainder;
-      $input .= str_repeat( '=', $padlen );
-    }
-    return base64_decode( strtr( $input, '-_', '+/' ) );
-  }
+	private function getUserInfo() {
+		if ( $this->userinfo === null ) {
+			$uri = new PhutilURI(
+				$this->getWikiPageURI( 'Special:OAuth/identify',
+					[ 'format' => 'json' ]
+				)
+			);
 
-  /** return true if hash1 has the same value as hash2 */
-  private function compareHash( $hash1, $hash2 ) {
-    $result = strlen( $hash1 ) ^ strlen( $hash2 );
-    $len = min( strlen( $hash1 ), strlen( $hash2) );
-    for ( $i = 0; $i < $len; $i++ ) {
-        $result |= ord( $hash1{$i} ) ^ ord( $hash2{$i} );
-    }
-    // this is just a constant time compare of the two hash strings
-    return $result == 0;
-  }
+			// We gen this so we can check for replay below:
+			$nonce = Filesystem::readRandomCharacters( 32 );
+
+			list( $body ) = $this->newOAuth1Future( $uri )
+				->setMethod( 'GET' )
+				->setNonce( $nonce )
+				->addHeader( 'User-Agent', __CLASS__ )
+				->resolvex();
+
+			$this->userinfo = $this->decodeAndVerifyJWT( $body, $nonce );
+		}
+
+		return $this->userinfo;
+	}
+
+	protected function willProcessTokenRequestResponse( $body ) {
+		if ( substr_count( $body, 'Error:' ) > 0 ) {
+			throw new Exception(
+				pht( 'OAuth provider returned an error response.' )
+			);
+		}
+	}
+
+	/**
+	 * MediaWiki uses a signed JWT to assert the user's identity
+	 * here we verify the identity, not just the jwt signature.
+	 */
+	private function decodeAndVerifyJWT( $jwt, $nonce ) {
+		$userinfo = [];
+		$identity = $this->decodeJWT( $jwt );
+		$iss_uri = new PhutilURI( $identity->iss );
+		$expected_uri = new PhutilURI( $this->mediaWikiBaseURI );
+
+		$now = time();
+
+		if ( $iss_uri->getDomain() !== $expected_uri->getDomain() ) {
+			throw new Exception(
+				pht( 'OAuth JWT iss didn\'t match expected server name' )
+			);
+		}
+
+		if ( $identity->aud !== $this->getConsumerKey() ) {
+			throw new Exception(
+				pht( 'OAuth JWT aud didn\'t match expected consumer key' )
+			);
+		}
+
+		if ( $identity->iat > $now || $identity->exp < $now ) {
+			throw new Exception(
+				pht( 'OAuth JWT wasn\'t valid at this time' )
+			);
+		}
+
+		if ( $identity->nonce !== $nonce ) {
+			throw new Exception(
+				pht( 'OAuth JWT nonce didn\'t match what we sent.' )
+			);
+		}
+
+		if ( $identity->blocked ) {
+			throw new Exception(
+				pht( 'OAuth error: this account has been blocked in MediaWiki.' )
+			);
+		}
+
+		$userinfo['userid'] = $identity->sub;
+		$userinfo['username'] = $identity->username;
+		$userinfo['groups'] = $identity->groups;
+		$userinfo['blocked'] = $identity->blocked;
+		$userinfo['editcount'] = $identity->editcount;
+
+		return $userinfo;
+	}
+
+	/** decode a JWT and verify the signature is valid */
+	private function decodeJWT( $jwt ) {
+		list( $headb64, $bodyb64, $sigb64 ) = explode( '.', $jwt );
+
+		$header = json_decode( $this->urlsafeB64Decode( $headb64 ) );
+		$body = json_decode( $this->urlsafeB64Decode( $bodyb64 ) );
+		$sig = $this->urlsafeB64Decode( $sigb64 );
+
+		$expect_sig = hash_hmac(
+			'sha256',
+			"$headb64.$bodyb64",
+			$this->getConsumerSecret()->openEnvelope(),
+			true
+		);
+
+		// MediaWiki will only use sha256 hmac (HS256) for now.
+		// This checks that an attacker doesn't return invalid JWT signature type.
+		if ( $header->alg !== 'HS256' ||
+			!$this->compareHash( $sig, $expect_sig )
+		) {
+			throw new Exception( 'Invalid JWT signature from /identify.' );
+		}
+
+		return $body;
+	}
+
+	private function urlsafeB64Decode( $input ) {
+		$remainder = strlen( $input ) % 4;
+
+		if ( $remainder ) {
+			$padlen = 4 - $remainder;
+			$input .= str_repeat( '=', $padlen );
+		}
+
+		return base64_decode( strtr( $input, '-_', '+/' ) );
+	}
+
+	/** return true if hash1 has the same value as hash2 */
+	private function compareHash( $hash1, $hash2 ) {
+		$result = strlen( $hash1 ) ^ strlen( $hash2 );
+		$len = min( strlen( $hash1 ), strlen( $hash2) );
+
+		for ( $i = 0; $i < $len; $i++ ) {
+				$result |= ord( $hash1{$i} ) ^ ord( $hash2{$i} );
+		}
+
+		// this is just a constant time compare of the two hash strings
+		return $result == 0;
+	}
 }
