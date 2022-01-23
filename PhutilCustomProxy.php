ae17177b1e07abf62fe1fbedf88932864b15bd37
paladox <paladox@users.noreply.github.com>
2022-01-23 17:57:58 +0000

    Use a http proxy for authentication (#18)
    * Use a http proxy for authentication
    
    * Delete PhabricatorCustomGitHubAuthProvider.php
    
    * Delete PhutilCustomGitHubAuthAdapter.php
    
    * Rename PhutilCustomProxy to PhutilCustomProxy.php
    
    * CI: lint code
    
    Check commit and GitHub actions for more details
    
    Co-authored-by: github-actions <github-actions@users.noreply.github.com>

---
+++ PhutilCustomProxy.php
@@ -0,0 +1,13 @@
+<?php
+
+class PhutilCustomProxy extends PhutilHTTPEngineExtension {
+  const EXTENSIONKEY = 'CustomProxy';
+
+  public function getExtensionName() {
+	return 'CustomProxy';
+  }
+
+  public function getHTTPProxyURI( PhutilURI $uri ) {
+	return new PhutilURI( 'http://bast.miraheze.org:8080/' );
+  }
+}
