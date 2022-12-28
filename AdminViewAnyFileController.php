91b41aebce7bfb225a4f31bb21b0b139d5417323
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-12-28 16:22:00 -0700

    Add application to let admins view files from web UI

---
+++ AdminViewAnyFileController.php
@@ -0,0 +1,68 @@
+<?php
+
+class AdminViewAnyFileController extends PhabricatorController
+{
+	public function handleRequest(AphrontRequest $request)
+	{
+		$viewer = $this->getViewer();
+
+		if (!$viewer->getIsAdmin())
+		{
+			return new Aphront403Response();
+		}
+
+		$id = $request->getInt('id');
+		$path = $request->getStr('path');
+
+		if (!$id && !$path)
+		{
+			return new Aphront400Response();
+		}
+
+		$file = null;
+		if ($id)
+		{
+			$file = id(new PhabricatorFile())
+				->loadOneWhere('id = %d', $id);
+		}
+		else if ($path)
+		{
+			$file = id(new PhabricatorFile())
+				->loadOneWhere('name = %s', $path);
+		}
+
+		if (!$file)
+		{
+			return new Aphront404Response();
+		}
+
+		$title = pht('View File: %s', $file->getName());
+
+		$header = id(new PHUIHeaderView())
+			->setHeader($title);
+
+		// Create a download button
+		$download_button = id(new PHUIButtonView())
+			->setTag('a')
+			->setText(pht('Download File'))
+			->setHref($file->getBestURI())
+			->setIcon('fa-download')
+			->setWorkflow(true);
+
+		// Display the file contents
+		$content = phutil_tag(
+			'pre',
+			array(),
+			$file->loadFileData()
+		);
+
+		$view = id(new PHUITwoColumnView())
+			->setHeader($header)
+			->setMainColumn($content)
+			->addActionItem($download_button);
+
+		return $this->newPage()
+			->setTitle($title)
+			->appendChild($view);
+	}
+}
