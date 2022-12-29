954219aae93c91f8db3d000e9dce13b3bfa69593
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-12-29 12:35:20 -0700

    Create AdminManageObjectsController.php

---
+++ AdminManageObjectsController.php
@@ -0,0 +1,20 @@
+<?php
+
+class AdminManageObjectsController extends PhabricatorController {
+
+	public function buildApplicationMenu() {
+		$menu = parent::buildApplicationMenu();
+
+		$viewer = $this->getViewer();
+		if ( $viewer->getIsAdmin() ) {
+			$item = id( new PHUIListItemView() )
+				->setName( pht( 'Change Any File Visibility' ) )
+				->setIcon( 'fa-file' )
+				->setHref( $this->getApplicationURI( 'file/' ) );
+
+			$menu->addMenuItem( $item );
+		}
+
+		return $menu;
+	}
+}
