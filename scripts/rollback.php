5a924b8532e9c1884e8c807db568f4d9635943dc
paladox <paladox@users.noreply.github.com>
2023-08-09 16:01:05 -0400

    Create rollback.php

---
+++ scripts/rollback.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * @license http://www.apache.org/licenses/ Apache License 2.0
+ */
+
+$root = dirname(dirname(__FILE__));
+require_once $root . '/scripts/init/init-script.php';
+init_script();
+$args = new PhutilArgumentParser($argv);
+$args->setTagline(pht('Phabricator transaction rollback tool.'));
+$args->setSynopsis(<<<EOSYNOPSIS
+**rollback** __workflow__ [__options__]
+    Roll back transactions
+
+EOSYNOPSIS
+);
+$args->parseStandardArguments();
+
+$workflows = id(new PhutilClassMapQuery())
+  ->setAncestorClass('MirahezeCLIWorkflow')
+  ->execute();
+$workflows[] = new PhutilHelpArgumentWorkflow();
+$args->parseWorkflows($workflows);
