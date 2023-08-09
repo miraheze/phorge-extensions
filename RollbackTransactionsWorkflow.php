c1d44ab77217880a37c3e77ece35b04cf9936be7
paladox <paladox@users.noreply.github.com>
2023-08-09 16:02:27 -0400

    Update RollbackTransactionsWorkflow.php

--- RollbackTransactionsWorkflow.php
+++ RollbackTransactionsWorkflow.php
@@ -1,8 +1,8 @@
 <?php
 
- /**
-  * @license http://www.apache.org/licenses/ Apache License 2.0
-  */
+/**
+ * @license http://www.apache.org/licenses/ Apache License 2.0
+ */
 class RollbackTransactionsWorkflow
 extends MirahezeCLIWorkflow
 {
