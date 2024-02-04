76478d4a721eaa2ebd20aa08070470abc6654223
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-03 17:10:35 -0700

    Phabricator -> Phorge

--- src/oauth/PhabricatorMediaWikiAuthProvider.php
+++ src/oauth/PhabricatorMediaWikiAuthProvider.php
@@ -81,12 +81,12 @@ final class PhabricatorMediaWikiAuthProvider extends PhabricatorOAuth1AuthProvid
 		if ( $this->isSetup() ) {
 			return pht(
 				"**Step 1 of 2**: Provide the name and URI for your MediaWiki install.\n\n" .
-				"In the next step, you will create an auth consumer in MediaWiki to be used by Phabricator oauth." );
+				"In the next step, you will create an auth consumer in MediaWiki to be used by Phorge oauth." );
 		} else {
 			$wiki_uri = $this->getWikiURI();
 
 			return pht(
-				"**Step 2 of 2**: Create a MediaWiki auth consumer for this Phabricator instance." .
+				"**Step 2 of 2**: Create a MediaWiki auth consumer for this Phorge instance." .
 				"\n\n" .
 				"NOTE: Propose a consumer with the form at this url: %s" .
 				"\n\n" .
@@ -96,10 +96,10 @@ final class PhabricatorMediaWikiAuthProvider extends PhabricatorOAuth1AuthProvid
 				"\n\n" .
 				"After you register the consumer, a **Consumer Key** and " .
 				"**Consumer Secret** will be provided to you by MediaWiki. " .
-				"To complete configuration of phabricator, copy the provided keys into " .
+				"To complete configuration of phorge, copy the provided keys into " .
 				"the corresponding fields above." .
 				"\n\n" .
-				"NOTE: Before Phabricator can successfully authenticate to your MediaWiki," .
+				"NOTE: Before Phorge can successfully authenticate to your MediaWiki," .
 				" a wiki admin must approve the oauth consumer registration using the form" .
 				" which can be found at the following url: %s",
 				$wiki_uri . '/index.php?title=Special:OAuthConsumerRegistration/propose',
