c7fc657954457ffb1473b47a9501c24755d323d7
paladox <paladox@users.noreply.github.com>
2025-03-28 19:49:11 +0000

    Include meta.miraheze.org and login.miraheze.org in OAuth CSP (#58)
    * Include meta.miraheze.org and login.miraheze.org in OAuth CSP
    
    Fixes T13443
    
    * CI: lint code to standards
    
    Check commit and GitHub actions for more details
    
    ---------
    
    Co-authored-by: github-actions <github-actions@users.noreply.github.com>

--- src/oauth/PhabricatorMediaWikiAuthProvider.php
+++ src/oauth/PhabricatorMediaWikiAuthProvider.php
@@ -297,6 +297,11 @@ final class PhabricatorMediaWikiAuthProvider extends PhabricatorOAuth1AuthProvid
 			$csp_actions[] = (string)$mobile_uri;
 		}
 
+		// WMF SUL specific domains, see T376803
+		if ( $uri->getDomain() == 'meta.miraheze.org' ) {
+			$csp_actions[] = 'https://login.miraheze.org/';
+		}
+
 		return $csp_actions;
 	}
 }
